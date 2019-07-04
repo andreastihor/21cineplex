7d814f1e8984544b4ba65e8ba9e3863d11319291
Andreas <t-andreas.tihor@ovo.id>
2019-07-04 11:40:30 +0700

    added two more routes

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 const Hapi=require('hapi');
-const service = require('./service.js')
-//test
+const {GetAllSchedule, } = require('./service.js')
+let constant = require('./constant')
 // Create a server with a host and port
 const server=Hapi.server({
     host:'localhost',
     port:8132
 });
+//
+// // Add the route
+// server.route({
+//     method:'GET',
+//     path:'/',
+//     handler: async (request,h) => {
+//       const response = await GetAllSchedule()
+//       constant = response
+//     }
+// });
 
-// Add the route
 server.route({
-    method:'GET',
-    path:'/',
-    handler: async (request,h) => {
-      const response = await service()
-      return response
-    }
-});
+  method : 'GET',
+  path : '/cinemaList',
+  handler : async(request,h) => {
+    return Object.keys(constant)
+  }
+})
+
+server.route({
+  method : 'GET',
+  path : '/movieList/{place}',
+  handler : async(request,h) => {
+    const x = request.params.place
+    return constant[x]
+  }
+})
 
 // Start the server
 const start =  async function() {
 
     try {
         await server.start();
-
+        const response = await GetAllSchedule()
+        constant = response
     }
     catch (err) {
         console.log(err);
@@ -33,3 +51,7 @@ const start =  async function() {
 };
 
 start();
+setInterval(async function()
+{  const response = await GetAllSchedule()
+  constant = response
+}, 86400000 )
