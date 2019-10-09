1877f41c0e29a1b3a2b37b4dce4dda35161c7fa0
Andreas <t-andreas.tihor@ovo.id>
2019-10-09 12:49:21 +0700

    added new server

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const puppeteer = require('puppeteer')
-const PUPPETEER_CONFIG = require('./puppeteer')
+const Glue = require('glue');
+const api = require('./api')
+
+const manifest = {
+  server: {
+    host: 'localhost',
+    port: 8112,
+    routes: {
+      cors: true,
+    },
+    router: {
+      stripTrailingSlash: true,
+    },
+  },
+  register: {
+    plugins: [
+      { plugin: api },
+    ],
+  },
+};
 
 
-async function createBrowser() {
-  const browser = await puppeteer.launch(PUPPETEER_CONFIG.launch);
-  const pages = await browser.pages();
-  const page = pages[0];
-  await page.setViewport(PUPPETEER_CONFIG.viewport);
-  page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36');
-  return {
-    page,
-    browser,
-  };
-}
 
-async function start() {
-  const {browser, page} = await createBrowser()
-  
-  await browser.close()
-}
+const startServer = async () => {
+  try {
+    const server = await Glue.compose(manifest, { relativeTo: __dirname });
+    await server.start();
+    console.log(`SERVER: Server running at ${server.info.uri}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
-
-start()
+startServer();
